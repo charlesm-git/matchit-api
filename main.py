9bc879ac9ec6584db41458d9575a9cbadb14f7d8
cmareau-git <charles_mareau@hotmail.fr>
2026-01-16 16:51:58 +0100

    Temporarily authorize access to everyone for debugging

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:8000",
-        "",
+        "*",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
